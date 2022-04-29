602a8680e85691749eafe332db33141765bf661e
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-04-29 10:54:00 -0300

    criando o sistema de cadastrar/adicionar produtos

---
+++ views/add.php
@@ -0,0 +1,16 @@
+<h1>Adicionar Produto</h1>
+
+<form method="POST">
+    Código de Barras:<br/>
+    <input type="text" name="cod"><br/><br/>
+    Nome do Produto:<br/>
+    <input type="text" name="name"><br/><br/>
+    Preço do Produto:<br/>
+    <input type="text" name="price"><br/><br/>
+    Quantidade:<br/>
+    <input type="text" name="quantity"><br/><br/>
+    Qtd. Mínima:<br/>
+    <input type="text" name="min_quantity"><br/><br/>
+
+    <input type="submit" value="Adicionar Produto">
+</form>
