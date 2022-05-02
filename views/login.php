345b26d9cdc349091e304e143b9875e918653915
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-05-02 14:28:37 -0300

    adicionando estilo minimo na tela de login

--- views/login.php
+++ views/login.php
@@ -1,12 +1,28 @@
-<meta charset="UTF-8">
-<form method="POST">
-    Seu número:<br/>
-    <input type="text" name="number"><br/><br/>
 
-    Sua senha:<br/>
-    <input type="password" name="password"><br/><br/>
-    <input type="submit" value="Entrar">
-</form> 
-<?php if(!empty($msg)): ?>
-<h2><?php echo$msg;?></h2>
-<?php endif; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>assets/css/login.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="loginarea">
+        <form method="POST">
+            Seu número:<br/>
+            <input type="text" name="number"><br/><br/>
+
+            Sua senha:<br/>
+            <input type="password" name="password"><br/><br/>
+            <input type="submit" value="Entrar">
+        </form> 
+    </div>
+    <?php if(!empty($msg)): ?>
+    <h2><?php echo$msg;?></h2>
+    <?php endif; ?>
+</body>
+</html>
+
+
