b798cda400065fbb757383952f755ca39b5c0a69
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-05-03 09:10:45 -0300

    projeto concluído!

--- views/add.php
+++ views/add.php
@@ -10,13 +10,13 @@
     Código de Barras:<br/>
     <input type="text" name="cod" required><br/><br/>
     Nome do Produto:<br/>
-    <input type="text" name="name" required><br/><br/>
+    <input type="text" name="name"  required><br/><br/>
     Preço do Produto:<br/>
-    <input type="text" name="price" required><br/><br/>
+    <input type="text" name="price" class="dinheiro" required><br/><br/>
     Quantidade:<br/>
-    <input type="text" name="quantity" required><br/><br/>
+    <input type="text" name="quantity" class="dinheiro" required><br/><br/>
     Qtd. Mínima:<br/>
-    <input type="text" name="min_quantity" required><br/><br/>
+    <input type="text" name="min_quantity" class="dinheiro" required><br/><br/>
 
     <input type="submit" value="Adicionar Produto">
 </form>
